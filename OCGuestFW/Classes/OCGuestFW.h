6be566ee4ae7dbe9f9345898ef4d6dd006993a3e
PrestigeApps <raul@prestigelab.es>
2019-04-24 18:11:44 +0200

    Upload test framework

--- OCGuestFW/Classes/OCGuestFW.h
+++ OCGuestFW/Classes/OCGuestFW.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char OCGuestFWVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <OCGuestFW/PublicHeader.h>
 
 #import <OCGuestFW/OCGuestFWViewController.h>
+#import <SeosMobileKeysSDK/SeosMobileKeysSDK.h>
