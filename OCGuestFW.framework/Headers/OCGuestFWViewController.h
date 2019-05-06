413f63b30213594502d03fab6656dd3495421a1a
PrestigeApps <raul@prestigelab.es>
2019-05-06 12:36:29 +0200

    Upload test framework

--- OCGuestFW.framework/Headers/OCGuestFWViewController.h
+++ OCGuestFW.framework/Headers/OCGuestFWViewController.h
@@ -16,42 +16,17 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface OCGuestFWViewController : UIViewController <CBCentralManagerDelegate, MobileKeysManagerDelegate>
-    typedef NS_ENUM(NSInteger, ViewState) {
-        NO_KEY = 0,
-        KEY_GENERATED_ERROR = 1,
-        KEY_GENERATED = 2,
-        OPEN_TRY = 3,
-        NO_BLUETOOTH = 4,
-        NO_KEY_DOOR = 5,
-        KEY_DOOR_OK = 6,
-        LOADING = 7
-    };
-
-    typedef NS_ENUM(NSInteger, LockType) {
-        Vingcard = 1,
-        VingcardD = 101,
-        Salto = 3,
-        SaltoD = 103
-    };
 
     typedef void (^integrationCallback)(NSString *result);
-
     @property (nonatomic, strong) integrationCallback callback;
-
     @property NSString *channelkey;
     @property NSString *roomstay_id;
     @property NSString *roomnumber;
     @property NSString *langcode;
 
-    - (IBAction)bt_key:(id)sender;
-
-    @property (weak, nonatomic) IBOutlet UILabel *m_lb_key;
-    @property (weak, nonatomic) IBOutlet UIButton *m_bt_getkey;
-    @property (weak, nonatomic) IBOutlet UIImageView *m_image;
-    @property (strong, nonatomic) IBOutlet UIView *m_View;
-    @property (weak, nonatomic) IBOutlet UIView *rectView;
-    @property (strong, nonatomic) CBCentralManager *centralManager;
-
+//    + (MobileKeysManager *)createInitializedMobileKeysManager: (OCGuestFWViewController *) delegate;
+    - (void) startASSAAbloyIntegration;
+    - (void) stopScanning;
 @end
 
 NS_ASSUME_NONNULL_END
